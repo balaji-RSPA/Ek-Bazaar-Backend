77b036ec326bcf7f5ede6f4a33c3e636f2d29722
ramesh <ramesh@active.agency>
2020-10-27 09:10:14 +0530

    API for chategories

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,7 +1,9 @@
 const location = require('./locationsModul')
 const buyers = require('./buyersModule')
+const category = require('./categoryModule')
 
 module.exports = {
     location,
-    buyers
+    buyers,
+    category
 }
--- src/routes/web/index.js
+++ src/routes/web/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router()
 const location = require('./locationRoutes')
 const buyer = require('./buyerRoutes')
+const category = require('./categotyRoutes')
 
 router.use(location)
 router.use(buyer)
+router.use(category)
 
 module.exports = router
