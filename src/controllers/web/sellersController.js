df4bedaea64109cb2850eaf27e37dc55eed151e4
Sreeraj <sreeraj@active.agency>
2020-11-24 10:51:00 +0530

    created api for seller delete product

--- src/controllers/web/sellersController.js
+++ src/controllers/web/sellersController.js
@@ -1,8 +1,9 @@
-const camelcaseKeys = require("camelcase-keys");
-const { machineIdSync } = require("node-machine-id");
-const { respSuccess, respError } = require("../../utils/respHadler");
+const camelcaseKeys = require('camelcase-keys')
+const { machineIdSync } = require('node-machine-id')
+const { respSuccess, respError } = require('../../utils/respHadler')
 
-const { sellers } = require("../../modules");
+const { sellers } = require('../../modules')
+
 const {
   updateSeller,
   getSeller,
@@ -17,21 +18,21 @@ const {
   sellerBulkInser,
   getSellerProfile,
   structureSellerData,
-} = sellers;
+  deleteSellerProduct,
+  getSellerVal,
+} = sellers
 
 module.exports.getSeller = async (req, res) => {
   try {
-    const { userID } = req;
-    const { id } = req.query;
-    const seller = userID
-      ? await getSeller(userID)
-      : await getSellerProfile(id);
-    console.log(seller, " dinal ressss");
-    respSuccess(res, seller);
+    const { userID } = req
+    const { id } = req.query
+    const seller = userID ? await getSeller(userID) : await getSellerProfile(id)
+    console.log(seller, ' dinal ressss')
+    respSuccess(res, seller)
   } catch (error) {
-    respError(res, error.message);
+    respError(res, error.message)
   }
-};
+}
 
 module.exports.updateSeller = async (req, res) => {
   try {
@@ -42,101 +43,130 @@ module.exports.updateSeller = async (req, res) => {
       establishmentPhotos,
       companyProfile,
       productDetails,
-      notifications
-    } = req.body;
-    const { userID } = req;
-    console.log(userID, "userID...................")
-    const user = await getSeller(userID);
-    console.log(user, ">>>>>>>>>>>>>.")
-    const sellerID = user._id;
-    let newData = {};
+      notifications,
+    } = req.body
+    const { userID } = req
+    console.log(userID, 'userID...................')
+    const user = await getSeller(userID)
+    console.log(user, '>>>>>>>>>>>>>.')
+    const sellerID = user._id
+    let newData = {}
     let seller
     // addProductDetails;
     if (businessDetails) {
-      const bsnsDtls = await addbusinessDetails(sellerID, businessDetails);
-      console.log(bsnsDtls, ";;;;;;;;");
-      newData.busenessId = bsnsDtls._id;
-      seller = await updateSeller({ _id: sellerID }, newData);
+      const bsnsDtls = await addbusinessDetails(sellerID, businessDetails)
+      console.log(bsnsDtls, ';;;;;;;;')
+      newData.busenessId = bsnsDtls._id
+      seller = await updateSeller({ _id: sellerID }, newData)
     }
     if (statutoryDetails) {
       const statutoryDtls = await addStatutoryDetails(
         sellerID,
-        statutoryDetails
-      );
-        console.log(statutoryDtls, ':::::::::')
-      newData.statutoryId = statutoryDtls._id;
-      seller = await updateSeller({ _id: sellerID }, newData);
+        statutoryDetails,
+      )
+      console.log(statutoryDtls, ':::::::::')
+      newData.statutoryId = statutoryDtls._id
+      seller = await updateSeller({ _id: sellerID }, newData)
     }
     if (contactDetails) {
-      const cntctDtls = await addContactDetails(sellerID, contactDetails);
+      const cntctDtls = await addContactDetails(sellerID, contactDetails)
       // newData.busenessId = cntctDtls._id;
     }
     if (establishmentPhotos) {
       const estblsmntPhts = await addEstablishmentPhotos(
         sellerID,
-        establishmentPhotos
-      );
-      newData.establishmentId = estblsmntPhts._id;
+        establishmentPhotos,
+      )
+      newData.establishmentId = estblsmntPhts._id
     }
     if (companyProfile) {
-      const cmpnyPrfl = await addCompanyDetails(sellerID, companyProfile);
-      newData.sellerCompanyId = cmpnyPrfl._id;
+      const cmpnyPrfl = await addCompanyDetails(sellerID, companyProfile)
+      newData.sellerCompanyId = cmpnyPrfl._id
     }
     if (productDetails) {
-      let productsId = [];
-
-      console.log(productDetails, "productDetails.......")
-      const prdctDtls = await addProductDetails(null, productDetails);
-      console.log(prdctDtls, "prdctDtls....................")
-      // const seller = await getSeller(id);
-      
-      productsId = user.sellerProductId;
-      console.log(productsId, "productsId........................")
-      if (productsId && productsId.length) {
-        productsId.push(prdctDtls._id);
-      } 
-      else {
-        productsId = []
-        productsId.push(prdctDtls._id);
+      let productsId = []
+      let prdctDtls
+      if (productDetails._id !== null) {
+        prdctDtls = await addProductDetails(productDetails._id, productDetails)
+      } else {
+        prdctDtls = await addProductDetails(null, productDetails)
       }
-      console.log(productsId)
-      newData.sellerProductId = productsId;
-      seller = await updateSeller({ _id: sellerID }, newData);
+      productsId = user.sellerProductId
+
+      if (
+        (productsId &&
+          productsId.length &&
+          productDetails &&
+          productDetails._id === null) ||
+        productDetails._id === undefined
+      ) {
+        productsId.push(prdctDtls._id)
+      } else if (
+        (prdctDtls._id &&
+          productsId.length === 0 &&
+          productDetails._id === null) ||
+        productDetails._id === undefined
+      ) {
+        productsId = []
+        productsId.push(prdctDtls._id)
+      }
+      newData.sellerProductId = productsId
+      seller = await updateSeller({ _id: sellerID }, newData)
     }
 
-    console.log(seller, " .........00000000000");
-    // const seller = await updateSeller({ _id: sellerID }, newData);
-    respSuccess(res, seller, "Profile updated successfully");
+    // console.log(seller, ' .........00000000000')
+    // const seller = await updateSeller({ _id: sellerID }, newData)
+    respSuccess(res, seller, 'Profile updated successfully')
   } catch (error) {
-    respError(res, error.message);
+    respError(res, error.message)
   }
-};
+}
 
 module.exports.getAllSellers = async (req, res) => {
   try {
-    const sellers = await getAllSellers();
-    respSuccess(res, sellers);
+    const sellers = await getAllSellers()
+    respSuccess(res, sellers)
   } catch (error) {
-    respError(res, error.message);
+    respError(res, error.message)
   }
-};
+}
 
 module.exports.sellerBulkInsert = async (req, res) => {
   try {
-    const reqData = req.body;
-    let bulkData = [];
-    let result;
+    const reqData = req.body
+    let bulkData = []
+    let result
     for (let index = 0; index < reqData.length; index++) {
-      const seller = reqData[index];
+      const seller = reqData[index]
       // const result = await inserSeller(seller)
-      result = await structureSellerData(seller);
+      result = await structureSellerData(seller)
       // bulkData.push(result)
     }
-    console.log("data upload completed");
+    console.log('data upload completed')
     // await sellerBulkInser(bulkData);
-    console.log("upload completed");
-    respSuccess(res, result);
+    console.log('upload completed')
+    respSuccess(res, result)
   } catch (error) {
-    respError(res, error.message);
+    respError(res, error.message)
   }
-};
+}
+
+module.exports.deleteSellerProduct = async (req, res) => {
+  try {
+    let result
+    let sellerProduct = await deleteSellerProduct(req.body.id)
+    let findSeller = await getSellerVal(sellerProduct.userID)
+    if (findSeller) {
+      let index =
+        findSeller.sellerProductId &&
+        findSeller.sellerProductId.indexOf(req.body.id)
+      if (index > -1) {
+        findSeller.sellerProductId = findSeller.sellerProductId.splice(index, 1)
+        await updateSeller({ _id: sellerProduct.userID }, findSeller)
+      }
+    }
+    respSuccess(res, result)
+  } catch (error) {
+    respError(res, error.message)
+  }
+}
--- src/modules/sellerProductModule.js
+++ src/modules/sellerProductModule.js
@@ -3,17 +3,15 @@ const SellerProducts = require('../models/sellerProductListSchema')
 
 module.exports.sellerProductsBulkInsert = (data) =>
   new Promise((resolve, reject) => {
-    SellerProducts.insertMany(data, function(err, result) {
-        if(err) reject(err)
-        else {
-            // console.log(result, ' product data---')
-            if(result){
-                const ids = result.map((val) => val._id)
-                resolve(ids)
-            }
-            reject(result)
+    SellerProducts.insertMany(data, function (err, result) {
+      if (err) reject(err)
+      else {
+        // console.log(result, ' product data---')
+        if (result) {
+          const ids = result.map((val) => val._id)
+          resolve(ids)
         }
+        reject(result)
+      }
     })
-    
-
-})
+  })
