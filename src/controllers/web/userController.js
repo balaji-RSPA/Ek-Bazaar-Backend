cab94de7d210b11f270746ae5da439833188e1e1
Ashutosh <ashutosh@active.agency>
2021-02-12 12:41:27 +0530

    changes for rfq popup

--- src/controllers/web/userController.js
+++ src/controllers/web/userController.js
@@ -110,8 +110,8 @@ module.exports.checkUserExistOrNot = async (req, res) => {
   try {
     const { mobile } = req.body;
     const seller = await checkUserExistOrNot({ mobile });
-    if (seller) {
-      respSuccess(res);
+    if (seller && seller.length) {
+      respSuccess(res, "User with number already exist");
     }
     respError(res, "No User found with this number");
   } catch (error) {
