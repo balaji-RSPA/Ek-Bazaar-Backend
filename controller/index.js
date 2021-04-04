2a5681c24dc7f1aeec0c57afd43ff696d977a37d
ramesh <ramesh@active.agency>
2021-04-04 17:05:25 +0530

    account deactivation changes

--- controller/index.js
+++ controller/index.js
@@ -263,7 +263,7 @@ const register = async (req, res, next) => {
       reason: ""
     }
   }
-
+  
   if (preferredLanguage) tenderUser.preferredLanguage = preferredLanguage;
   const user = await UserModel.findOneAndUpdate({ mobile: mobile.mobile || mobile }, { $set: tenderUser }, { new: true, upsert: true }); //.exec()
   if (!user) {
@@ -281,7 +281,7 @@ const register = async (req, res, next) => {
       },
     },
   };
-
+  
   // console.log("🚀 ~ file: index.js ~ line 235 ~ register ~ url", url);
   const { serviceURL } = req.query;
   const id = encodedId();
@@ -291,7 +291,7 @@ const register = async (req, res, next) => {
     return res.redirect("/");
   }
   const _url = new URL(serviceURL);
-
+  
   const intrmid = encodedId();
   storeApplicationInCache(_url.origin, id, intrmid);
   const response = await axios({
@@ -309,6 +309,7 @@ const register = async (req, res, next) => {
       origin,
     },
   });
+  console.log('1111111111111111111111111')
   const { data } = response;
   console.log("🚀 ~ file: index.js ~ line 251 ~ register ~ data", data);
   req.session.token = data.data.token;
@@ -318,7 +319,7 @@ const register = async (req, res, next) => {
     { token: data.data.token, _user: req.session.user },
     data.message
   );
-  // return res.send({ user, url: `${serviceURL}?ssoToken=${intrmid}` });
+  return res.send({ user, url: `${serviceURL}?ssoToken=${intrmid}` });
 };
 
 const doLogin = async (req, res, next) => {
@@ -620,7 +621,7 @@ const postRFP = async (req, res, next) => {
     if (response.data && response.data.data && response.data.data.token) req.session.token = response.data.data.token
     return respSuccess(res, { ...response.data.data }, response.data.message);
   } else {
-    return respError(res, "Somthing went wrong");
+    return respError(res, response.data && response.data.message ? response.data.message : "Somthing went wrong");
   }
 };
 
