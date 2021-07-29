410554baefa8843ec8fac2686264c02c1dcfb71e
ramesh <ramesh@active.agency>
2021-07-29 17:57:48 +0530

    log

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ require('./config/tenderdb').conn
 const Logger = require('./src/utils/logger');
 const config = require('./config/config')
 const { sendQueSms, getExpirePlansCron, sendQueEmails, getAboutToExpirePlan } = require('./src/crons/cron')
-const { updatePriority } = require('./src/controllers/web/testController')
+const { updatePriority, gujaratSellerData } = require('./src/controllers/web/testController')
 const { respSuccess, respError } = require("./src/utils/respHadler")
 const router = require('./src/routes');
 const { request } = require("./src/utils/request")
@@ -112,6 +112,14 @@ app.get("/deleteTestData", async function (req, res) {
   } catch (error) { }
   // res.send('Its delete records  live')
 });
+app.get("/gujaratSellerData", async function (req, res) {
+  try {
+    const result = await gujaratSellerData(req, res)
+  } catch (error) { }
+  // res.send('Its delete records  live')
+});
+
+
 
 async function indexing() {
   await checkIndices();
