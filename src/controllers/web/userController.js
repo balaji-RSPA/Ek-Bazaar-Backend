39c1d7782de07a60226a26399e67166f150a2618
ramesh <ramesh@active.agency>
2021-07-30 17:57:24 +0530

    logo

--- src/controllers/web/userController.js
+++ src/controllers/web/userController.js
@@ -186,7 +186,7 @@ module.exports.sendOtp = async (req, res) => {
       seller[0].email &&
       seller[0].isEmailVerified === 2;
 
-    if (!isProd) {
+    if (isProd) {
       otp = Math.floor(1000 + Math.random() * 9000);
       otpMessage = otpVerification({ otp });
       if (mobile) {
