ea73beceec128f90eade0d369af8e0f8265de71a
Ravinder <ravinder@active.agency>
2022-04-07 09:09:47 +0000

    commit server changes before search functionality changes

--- app.js
+++ app.js
@@ -57,7 +57,8 @@ app.use(
       "http://admin.ekbazaar.tech-active.com",
       "https://admin.ekbazaar.tech-active.com",
       "http://192.168.1.28:8086",
-      "http://192.168.1.74:8086"
+      "http://192.168.1.74:8086",
+      "http://192.168.1.199:8086"
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD"],
     credentials: true,
